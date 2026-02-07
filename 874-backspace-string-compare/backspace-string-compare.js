465f08b07bf26ee186da66bcb412936c91e1991c
Segni Tsega Banjew <segnitsega6@gmail.com>
2026-02-07 23:30:47 +0300

    Time: 0 ms (100.00%) | Memory: 55.5 MB (21.77%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.js
+++ 874-backspace-string-compare/backspace-string-compare.js
@@ -3,9 +3,9 @@ var backspaceCompare = function(s, t) {
   let n2 = 0;
   let p1 = s.length - 1;
   let p2 = t.length - 1;
-  if (s[p1] !== "#" && t[p2] !== "#") {
-    if (s[p1] !== t[p2]) return false;
-  }
+//   if (s[p1] !== "#" && t[p2] !== "#") {
+//     if (s[p1] !== t[p2]) return false;
+//   }
 
   while (p1 >= 0 || p2 >= 0) {
     if (s[p1] !== "#" && t[p2] !== "#") {
