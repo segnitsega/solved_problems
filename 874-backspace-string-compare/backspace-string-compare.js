72fac1e6b9ce23fdc1f25123afd543c6d4fd17ef
Segni Tsega Banjew <segnitsega6@gmail.com>
2026-02-07 23:23:04 +0300

    Time: 0 ms (100.00%) | Memory: 54.2 MB (65.43%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.js
+++ 874-backspace-string-compare/backspace-string-compare.js
@@ -1,23 +1,39 @@
 var backspaceCompare = function(s, t) {
-    let typedS = [];
-    let typedT = [];
-    for(let i = 0; i < s.length; i++){
-        if(s[i] != "#"){
-            typedS.push(s[i]);
-        } else{
-            typedS.pop()
-        }
+  let n1 = 0;
+  let n2 = 0;
+  let p1 = s.length - 1;
+  let p2 = t.length - 1;
+  if (s[p1] !== "#" && t[p2] !== "#") {
+    if (s[p1] !== t[p2]) return false;
+  }
+
+  while (p1 >= 0 || p2 >= 0) {
+    if (s[p1] !== "#" && t[p2] !== "#") {
+      if (n1 === 0 && n2 === 0) {
+        if (s[p1] !== t[p2]) return false;
+        p1--;
+        p2--;
+      } else if (n1 > 0 && n2 > 0) {
+        p1--;
+        n1--;
+        p2--;
+        n2--;
+      } else if (n1 > 0) {
+        p1--;
+        n1--;
+      } else if (n2 > 0) {
+        p2--;
+        n2--;
+      }
+    } else if (s[p1] === "#") {
+      n1 += 2;
+      p1--;
+      n1--;
+    } else if (t[p2] === "#") {
+      n2 += 2;
+      p2--;
+      n2--;
     }
-     for(let i = 0; i < t.length; i++){
-        if(t[i] != "#"){
-            typedT.push(t[i]);
-        } else{
-            typedT.pop()
-        }
-    }
-    if(typedS.length != typedT.length) return false;
-    for(let i = 0; i < typedS.length; i++){
-        if(typedS[i] != typedT[i]) return false;
-    }
-    return true;
+  }
+  return true;
 };
