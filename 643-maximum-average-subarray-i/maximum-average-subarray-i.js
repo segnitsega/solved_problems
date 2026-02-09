c73cfb1d2845e660dcb82aae246ddbc924fed7e6
Segni Tsega Banjew <segnitsega6@gmail.com>
2026-02-09 20:43:46 +0300

    Time: 3 ms (54.92%) | Memory: 69.6 MB (13.36%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.js
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.js
@@ -1,19 +1,20 @@
 var findMaxAverage = function(nums, k) {
-  if (nums.length === 0) return 0;
-  if (nums.length === 1) return nums[0] / k;
-
-  let p1 = 0;
+  if(nums.length === 0) return 0;
+  if (nums.length === 1) return nums[0]/k; 
+  let sum = 0;
   let maxAvg = -Infinity;
-  while (p1 <= nums.length - k) {
-    let p2 = p1 + k;
-    let sum = 0;
-    let currentAvg = 0;
-    for (let i = p1; i < p2; i++) {
-      sum += nums[i];
-    }
+  for (let i = 0; i < k; i++) {
+    sum += nums[i];
+  }
+  let currentAvg = sum / k;
+  maxAvg = currentAvg;
+
+  let p = 1;
+  while (p <= nums.length - k) {
+    sum = sum - nums[p - 1] + nums[p + k - 1];
     currentAvg = sum / k;
-    maxAvg = Math.max(maxAvg, currentAvg);
-    p1++;
+    maxAvg = Math.max(currentAvg, maxAvg);
+    p++;
   }
   return maxAvg;
 };
